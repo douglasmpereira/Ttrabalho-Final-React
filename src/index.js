724b953fcaafe0c7898d4077c4da3ba6ecc969cd
Losath <mar.oliveirabreder@gmail.com>
2022-06-20 16:50:11 -0300

    Router preparado no index

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route, Navigate }  from 'react-router-dom';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/login' element={<Login />}/>
+        <Route path='/painel' element={<Painel />}/>
+      </Routes>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
