bb587a8ccce3fd030ece6d8d8d05eea143b79d28
Thatto Salvini <thattosalvini@outlook.com>
2022-06-22 19:27:28 -0300

    update

---
+++ src/Routes/index.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import User from "../Pages/user";
+import Home from "../Pages/home";
+import Login from "../Pages/login";
+import { AuthContext } from "../../src/contexts/auth";
+//import ProtectedRoutes from "../ProtectedRoutes";
+
+const Rotas =()=> {
+    const [usuario, setUsuario] = useState(null)
+    
+    
+    const logar = (login, senha) => {
+        console.log("login auth", {login, senha})
+        setUsuario({id: "123", senha})
+    }
+
+    const logout = () => {
+        console.log("logout")
+    }
+
+
+    return(
+        <BrowserRouter>
+            <AuthContext.Provider 
+            value={{authenticated:
+            !!usuario, usuario, logar, logout}}>
+                <Routes>
+                    <Route path='/' element={<Home />}/>
+                    <Route path='/home' element={<Home />}/>
+                    <Route path='/login' element={<Login />}/>
+                    {usuario && <Route path='/user' element={<User />}/>}
+                    <Route path='*' element={usuario ? <User/> : <Navigate to= '/login' />} />
+                </Routes>
+            </AuthContext.Provider>
+        </BrowserRouter>    
+    )
+}
+
+export default Rotas
